44a72212804cea1cf29ba575a2c55f58bc4a5b54
lc <15365811995@163.com>
2020-12-12 23:11:07 +0800

    add convnet weight norm

--- ann_normalize.py
+++ ann_normalize.py
@@ -13,17 +13,26 @@ import data_preprocessing
 
 
 fcn_model_path = os.path.join(".", "ann_model_descs/fcn_model.h5")
+conv_model_path = os.path.join(".", "ann_model_descs/conv_model.h5")
 fcn_norm_model_path = os.path.join(".", "ann_model_descs/fcn_normed_model.h5")
+conv_norm_model_path = os.path.join(
+    ".", "ann_model_descs/conv_normed_model.h5")
+
+TYPE_FCN = 0
+TYPE_CONV = 1
 
 
-def load_model() -> keras.models.Model:
+def load_model(model_type: int) -> keras.models.Model:
     """
     Load already trainned model
     """
-    return keras.models.load_model(fcn_model_path)
+    if model_type == TYPE_FCN:
+        return keras.models.load_model(fcn_model_path)
+    else:
+        return keras.models.load_model(conv_model_path)
 
 
-def fcn_param_normalization(model: keras.Model, dataX: np.array) -> keras.Model:
+def param_normalization(model: keras.Model, dataX: np.array) -> keras.Model:
     previous_factor = 1
     beg_layer_idx = 0
     if model.layers[0].__class__.__name__ == "InputLayer":
@@ -32,6 +41,8 @@ def fcn_param_normalization(model: keras.Model, dataX: np.array) -> keras.Model:
     for i in range(beg_layer_idx, len(model.layers)):
         print("Current processing layer index {}, layer name {}".format(
             i, model.layers[i].__class__.__name__))
+        if len(model.layers[i].get_weights()) <= 1:
+            continue
         max_wt, max_act = 0, 0
         input_weights = deepcopy(np.array(model.layers[i].get_weights()))
         max_wt = max(max_wt, np.max(input_weights))
@@ -46,7 +57,7 @@ def fcn_param_normalization(model: keras.Model, dataX: np.array) -> keras.Model:
     return model
 
 
-def test_fcn_ann(model: keras.models.Model, dataX: np.array, dataY: np.array):
+def test_ann(model: keras.models.Model, dataX: np.array, dataY: np.array):
     print("Start testing parameter normalization performace...")
     preds = model.predict(dataX, batch_size=1, verbose=1)
     preds = np.argmax(preds, axis=-1)
@@ -59,8 +70,18 @@ def save_normed_model(model: keras.models.Model):
 
 
 if __name__ == "__main__":
-    model = load_model()
-    dataX, dataY = data_preprocessing.load_mnist_dataset()
-    normed_model = fcn_param_normalization(model, dataX[-50:])
-    test_fcn_ann(normed_model, dataX[:100], dataY[:100])
-    save_normed_model(normed_model)
+    # full connected network test
+    # -------------------------
+    # model = load_model()
+    # dataX, dataY = data_preprocessing.load_mnist_dataset()
+    # normed_model = param_normalization(model, dataX[-50:])
+    # test_ann(normed_model, dataX[:100], dataY[:100])
+    # save_normed_model(normed_model)
+
+    # conv model network test
+    # -------------------------
+    model = load_model(TYPE_CONV)
+    dataX, dataY = data_preprocessing.load_mnist_dataset(
+        data_need_flatten=False)
+    normed_model = param_normalization(model, dataX[-50:])
+    test_ann(normed_model, dataX[:100], dataY[:100])
