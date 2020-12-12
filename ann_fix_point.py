512438d42c4121c245040de88a290d9ccc728c8e
lc <15365811995@163.com>
2020-12-12 16:31:06 +0800

    finish fcn conversion

---
+++ ann_fix_point.py
@@ -0,0 +1,80 @@
+# -*- coding:utf-8 -*-
+"""
+Convert the float point parameters of ANN model to fix point bitwidth
+After weight normalization and tested, this file provide method for 
+converting the float point weights to fix point
+
+the weight magnitudes are in [-2^{BIT_WIDTH_WEIGHT-1}, 2^{BIT_WIDTH_WEIGHT-1}-1]
+the weight manitudes width of weight need to be e.g. 2 or 4 or 6 ... 32 and the
+bit width for activation output is alike
+"""
+import keras
+import numpy as np
+import os
+import ann_normalize
+from copy import deepcopy
+import data_preprocessing
+
+BIT_WIDTH_WEIGHT_MIN = 2
+BIT_WIDTH_WEIGHT_MAX = 32
+
+fcn_norm_fix_model_path = os.path.join(
+    ".", "ann_model_descs/fcn_normed_fixed_model.h5")
+
+
+def load_model() -> keras.models.Model:
+    return keras.models.load_model(ann_normalize.fcn_norm_model_path)
+
+
+def fix_point(model: keras.models.Model, bit_width_weight: int, dataX: np.array) -> keras.models.Model:
+    if bit_width_weight < BIT_WIDTH_WEIGHT_MIN or bit_width_weight > BIT_WIDTH_WEIGHT_MAX:
+        raise AssertionError(
+            "Bit width is not valid, assume to be 2,4,6,8 or alike")
+
+    beg_idx = 0
+    if model.layers[0].__class__.__name__ == "InputLayer":
+        beg_idx = 1
+
+    scale_bottom = -(2**(bit_width_weight-1))
+    scale_up = (2**(bit_width_weight-1))-1
+    layer_weights = []
+    for i in range(beg_idx, len(model.layers)):
+        layer_weights.append(
+            deepcopy(np.array(model.layers[i].get_weights(), dtype="float32")))
+
+    max_wt = max([np.max(wts) for wts in layer_weights])
+    min_wt = min([np.min(wts[wts >= 0]) for wts in layer_weights])
+    quant_factor = scale_up/(max_wt-min_wt)
+    for i in range(len(layer_weights)):
+        layer_weights[i] = np.floor(quant_factor*(layer_weights[i]-min_wt))
+
+    max_wt_neg = min([np.min(wts[wts < 0]) for wts in layer_weights])
+    min_wt_neg = max([np.max(wts[wts < 0]) for wts in layer_weights])
+    quant_factor = scale_bottom/(max_wt_neg-min_wt_neg)
+    for i in range(len(layer_weights)):
+        layer_weights[i] = np.ceil(quant_factor*(layer_weights[i]-min_wt))
+
+    for i in range(beg_idx, len(model.layers)):
+        model.layers[i].set_weights(layer_weights[i-beg_idx])
+
+    return model
+
+
+def test_model(model: keras.models.Model, dataX: np.array, dataY: np.array):
+    preds = model.predict(dataX, batch_size=1, verbose=1)
+    preds = np.argmax(preds, axis=-1)
+    accu = np.sum(preds == np.argmax(dataY, axis=-1))/len(preds)
+    print(
+        "After convert to fix point integer, accuracy is {:.2%}".format(accu))
+
+
+def save_model(model: keras.models.Model):
+    model.save(fcn_norm_fix_model_path)
+
+
+if __name__ == "__main__":
+    model = load_model()
+    dataX, dataY = data_preprocessing.load_mnist_dataset()
+    fixed_point_model = fix_point(model, 16, dataX[-10:])
+    test_model(fixed_point_model, dataX[-100:], dataY[-100:])
+    save_model(fixed_point_model)
---
+++ ann_normalize.py
@@ -0,0 +1,66 @@
+# -*- coding:utf-8 -*-
+"""
+Parameter normlization
+Load trained model of ann, feed with sample data and 
+apply parameter normalization on original ann network
+"""
+import keras
+import numpy as np
+import os
+import fcn
+from copy import deepcopy
+import data_preprocessing
+
+
+fcn_model_path = os.path.join(".", "ann_model_descs/fcn_model.h5")
+fcn_norm_model_path = os.path.join(".", "ann_model_descs/fcn_normed_model.h5")
+
+
+def load_model() -> keras.models.Model:
+    """
+    Load already trainned model
+    """
+    return keras.models.load_model(fcn_model_path)
+
+
+def fcn_param_normalization(model: keras.Model, dataX: np.array) -> keras.Model:
+    previous_factor = 1
+    beg_layer_idx = 0
+    if model.layers[0].__class__.__name__ == "InputLayer":
+        beg_layer_idx = 1
+    print("Start parameter normalization...")
+    for i in range(beg_layer_idx, len(model.layers)):
+        print("Current processing layer index {}, layer name {}".format(
+            i, model.layers[i].__class__.__name__))
+        max_wt, max_act = 0, 0
+        input_weights = deepcopy(np.array(model.layers[i].get_weights()))
+        max_wt = max(max_wt, np.max(input_weights))
+        act_out = keras.models.Model(inputs=model.input, outputs=model.layers[i].output).predict(
+            dataX, batch_size=1, verbose=1)
+        max_act = max(max_act, np.max(act_out))
+        scale_factor = max(max_wt, max_act)
+        applied_factor = scale_factor/previous_factor
+        model.layers[i].set_weights(input_weights/applied_factor)
+        previous_factor = scale_factor
+
+    return model
+
+
+def test_fcn_ann(model: keras.models.Model, dataX: np.array, dataY: np.array):
+    print("Start testing parameter normalization performace...")
+    preds = model.predict(dataX, batch_size=1, verbose=1)
+    preds = np.argmax(preds, axis=-1)
+    accu = np.sum(preds == np.argmax(dataY, axis=-1))/len(preds)
+    print("After parameter normalization, accuracy is {:.2%}".format(accu))
+
+
+def save_normed_model(model: keras.models.Model):
+    model.save(fcn_norm_model_path)
+
+
+if __name__ == "__main__":
+    model = load_model()
+    dataX, dataY = data_preprocessing.load_mnist_dataset()
+    normed_model = fcn_param_normalization(model, dataX[-50:])
+    test_fcn_ann(normed_model, dataX[:100], dataY[:100])
+    save_normed_model(normed_model)
